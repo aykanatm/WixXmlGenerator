e7a07010e1e6c1d7ce3a73f7bf3fec6a49929d59
Murat Aykanat <aykanatm@gmail.com>
2017-11-04 15:24:47 +0300

    Generate command first iteration.

--- WixXmlGenerator/WixXmlGenerator/Commands/GenerateCommand.cs
+++ WixXmlGenerator/WixXmlGenerator/Commands/GenerateCommand.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using WixXmlGenerator.Statics;
 
 namespace WixXmlGenerator.Commands
 {
@@ -11,7 +14,52 @@ namespace WixXmlGenerator.Commands
 
         public string Execute(List<string> args)
         {
-            return "Executed.";
+            try
+            {
+                var sourceDirArg = args[1];
+                var sourceDir = args[2];
+                var outputFileArg = args[3];
+                var outputFile = args[4];
+
+                if (sourceDirArg == Arguments.SourceDir && outputFileArg == Arguments.OutputFile)
+                {
+                    if (Directory.Exists(sourceDir))
+                    {
+                        if (File.Exists(outputFile))
+                        {
+                            Console.WriteLine();
+                            Console.Write("File path '" + outputFile + "' already exists. Do you wish to overwrite this file? [Y/N]: ");
+                            var response = (char) Console.Read();
+                            if (response == 'y' || response == 'Y')
+                            {
+                                Wix.WixXmlGenerator.Generate(sourceDir, outputFile);
+                            }
+                            else if (response == 'n' || response == 'N')
+                            {
+                                return "Please re-run the tool with a different output file path.";
+                            }
+                            else
+                            {
+                                throw new Exception("Response '" + response + "' is not a valid response.");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("Directory path " + sourceDir + " is not valid.");
+                    }
+                }
+                else
+                {
+                    throw new Exception("One or more of the provided arguments not recognized.");
+                }
+
+                return "Output file successfully created at '" + outputFile + "'.";
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
     }
 }
